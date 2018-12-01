f7ceace96bfceda5ba1381a952bdf43041a6f7bd
AJTech2002 <bestbatchprogrammer101@gmail.com>
2018-12-01 17:01:27 +1000

    Player Recording Update
    -> Player Recording Complete
    -> Will move onto gameplay mechanics next

---
+++ LDJam/Assets/ConsistentOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsistentOffset : MonoBehaviour
+{
+
+    public Transform offsetTransform;
+    public Vector3 offsetVector;
+    public float speed;
+
+    private void LateUpdate() {
+        transform.position = offsetTransform.position+offsetVector;
+    }
+
+
+}
